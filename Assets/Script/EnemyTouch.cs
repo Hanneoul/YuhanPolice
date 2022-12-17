332f71ee6a4cbe992c878d4ec46304a309b7d3a2
Taesuuu <82026757+Taesuuu@users.noreply.github.com>
2022-12-17 21:40:19 +0900

    터치 이펙트 변경 완료

--- Assets/Script/EnemyTouch.cs
+++ Assets/Script/EnemyTouch.cs
@@ -11,35 +11,61 @@ public class EnemyTouch : MonoBehaviour, IPointerClickHandler
     
     public bool isTouch;
     public bool isEnd;
+
+    public float time;
+    private Vector2 startPos;
     // Start is called before the first frame update
     void Start()
     {
-        frontImage.fillAmount = 0;
+        // frontImage.fillAmount = 0;
+        startPos = frontImage.rectTransform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        value += (Time.deltaTime);
-        frontImage.fillAmount = value / 5;
-        if(frontImage.fillAmount == 1) {
+        // value += (Time.deltaTime);
+        // frontImage.fillAmount = value / 5;
+        // if(frontImage.fillAmount == 1) {
+        //     GameManager._instance.EnemyTouchTimeOver();
+        //     GameManager._instance.isTimeout = true;
+        //     GameManager._instance.isTouchFailed = true;
+        //     Destroy(this.gameObject);
+        // }
+
+        frontImage.rectTransform.localScale = startPos * ((time / 2) - 1 );
+        time += Time.deltaTime;
+        if (frontImage.rectTransform.localScale.x >= 0.1f)
+        {
+            time = 0;
             GameManager._instance.EnemyTouchTimeOver();
             GameManager._instance.isTimeout = true;
             GameManager._instance.isTouchFailed = true;
             Destroy(this.gameObject);
+            
         }
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("Touch");
-        GameManager._instance.isTouch = true;
-        GameManager._instance.isEenmy = false;
-        Destroy(this.gameObject);
-        isTouch = true;
-        GameManager._instance.EnemyTouch();
-        // todo 원래 자리로 돌아가는 로직
+        if(time >= 0.8f && time < 1.2f){
+            Debug.Log("Touch");
+            GameManager._instance.isTouch = true;
+            GameManager._instance.isEenmy = false;
+            Destroy(this.gameObject);
+            isTouch = true;
+            GameManager._instance.EnemyTouch();
+            // todo 원래 자리로 돌아가는 로직
+        }
+        else {
+            time = 0;
+            GameManager._instance.EnemyTouchTimeOver();
+            GameManager._instance.isTimeout = true;
+            GameManager._instance.isTouchFailed = true;
+            Destroy(this.gameObject);
+        }
+        
     }
 
 
